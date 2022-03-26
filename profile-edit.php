1694fa0e6d2511e62b6361f9295aca4ec77a868c
Thet Minn Htun <59432845+thetminnhtun-scm@users.noreply.github.com>
2022-03-26 10:01:59 +0630

    Update profile

--- profile-edit.php
+++ profile-edit.php
@@ -5,11 +5,32 @@ if(!isset($_SESSION['auth'])) {
     redirect('login.php');
 }
 
+$id = $_SESSION['auth']['id'];
+$sql = "SELECT * FROM users WHERE id='$id'";
+$result = mysqli_query($conn, $sql);
+$user = mysqli_fetch_assoc($result);
+
 $errors = [];
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // validate name
-    // update set name="$name", password="$pasword" where id="$id"
+    $name = $_POST['name'];
+    $password = $_POST['password'];
+
+    if (!$name) {
+        $errors['name'] = 'The name is required.';
+    }
+
+    if(count($errors) == 0) {
+        if($password) {
+            $sql = "UPDATE users SET `name`='$name', `password`='$password' where `id`='$id'";
+        } else {
+            $sql = "UPDATE users SET `name`='$name' where `id`='$id'";
+        }
+    
+        $result = mysqli_query($conn, $sql);
+
+        redirect('profile-edit.php');
+    }
 }
 
 ?>
@@ -22,29 +43,34 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <div class="card mt-5">
                 <div class="card-body">
                 <form method="POST">
-                    <input type="hidden" name="id" value="<?php echo $_SESSION['auth']['id']; ?>">
+                    <div>
+                        <input 
+                            type="text" 
+                            name="name" 
+                            value="<?php echo $user['name']; ?>"
+                            class="form-control <?php if (isset($errors['name'])): ?> is-invalid <?php endif;?>" 
+                            placeholder="Enter name">
+                            <?php if (isset($errors['name'])): ?>
+                                <div class="invalid-feedback"><?php echo $errors['name']; ?></div>
+                            <?php endif;?>
+                    </div>
+ 
+                    <div class="mt-3">
+                        <input 
+                            type="password" 
+                            name="password" 
+                            class="form-control <?php if (isset($errors['password'])): ?> is-invalid <?php endif;?>" 
+                            placeholder="Enter password">
+                            <?php if (isset($errors['password'])): ?>
+                                <div class="invalid-feedback"><?php echo $errors['password']; ?></div>
+                            <?php endif;?>
+                    </div>
 
-                            <div>
-                                <input type="text" name="name" value="<?php echo $_SESSION['auth']['name']; ?>"
-                                 class="form-control <?php if (isset($errors['name'])): ?> is-invalid <?php endif;?>" 
-                                 placeholder="Enter name">
-                                 <?php if (isset($errors['name'])): ?>
-                                     <div class="invalid-feedback"><?php echo $errors['name']; ?></div>
-                                 <?php endif;?>
-                            </div>
- 
-                            <div class="mt-3">
-                             <input type="password" name="password" class="form-control <?php if (isset($errors['password'])): ?> is-invalid <?php endif;?>" placeholder="Enter password">
-                                 <?php if (isset($errors['password'])): ?>
-                                     <div class="invalid-feedback"><?php echo $errors['password']; ?></div>
-                                 <?php endif;?>
-                            </div>
- 
-                             <div class="d-flex justify-content-between mt-3">
-                                <button type="submit" class="btn btn-primary">Update</button>
-                                <a href="/blog/profile.php" class="btn btn-outline-secondary">Cancel</a>
-                             </div>
-                         </form>
+                        <div class="d-flex justify-content-between mt-3">
+                        <button type="submit" class="btn btn-primary">Update</button>
+                        <a href="/blog/profile.php" class="btn btn-outline-secondary">Cancel</a>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
